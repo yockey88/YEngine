02befbe783638a4edcf5c1ac9c2e99041322ca91
Yock <64506168+yockey88@users.noreply.github.com>
2022-11-19 11:15:35 -0700

    cross platform buildsln

--- tools/buildsln.py
+++ tools/buildsln.py
@@ -5,3 +5,9 @@ if globals.IsWindows():
     VS_BUILD_PATH = os.environ["VS_BUILD_PATH"][8:-1]
     VS_BUILD_PATH = "C:\\\\" + VS_BUILD_PATH
     subprocess.call(["cmd.exe" , "/c" , VS_BUILD_PATH , "{}.sln".format(globals.PROJECT_NAME) , "/property:Configuration={}".format(globals.CONFIG)])
+
+if globals.IsLinux():
+    subprocess.call(["make" , "config={}".format(globals.CONFIG)])
+
+if globals.IsMac():
+    subprocess.call(["make" , "config={}".format(globals.CONFIG)])
